350bfabc36eb9afb8c29d4a1852ee3fc2576d61c
ProfEnFrance <polliv44@gmail.com>
2025-06-06 16:00:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL") or "sqlite:///temp.db"
 db = SQLAlchemy(app)
 
 class Answer(db.Model):
